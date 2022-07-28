869d4f6255e8911df68256b733cff146b4b11b9b
Tawashiy <97021226+Tawashiy@users.noreply.github.com>
2022-07-28 22:04:59 +0900

    「セクション8」まで

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import axios from "axios";
 import "./styles.css";
 
 export const App = () => {
   const onClickUsers = () => {
-    alert();
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
   const onClickUser1 = () => {
-    alert();
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="App">
